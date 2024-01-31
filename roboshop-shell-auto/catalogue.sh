0f4818c5d6d61dfba03f9589e902e64d27ddf912
anjin <awsdpr.anji01@gmail.com>
2024-02-01 05:20:12 +0800

    catalogue script and config

--- roboshop-shell-auto/catalogue.sh
+++ roboshop-shell-auto/catalogue.sh
@@ -1,36 +1,36 @@
 
-echo "\e[33m Module list \e[0m"
+echo -e "\e[33m Module list \e[0m"
 dnf module disable nodejs -y &>>/tmp/roboshop.log
 dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
 
-echo "\e[33m  install NodeJs \e[0m"
+echo -e "\e[33m  install NodeJs \e[0m"
 dnf install nodejs -y &>>/tmp/roboshop.log
 
-echo "\e[33m  add app user \e[0m"
+echo -e "\e[33m  add app user \e[0m"
 rm -rf useradd &>>/tmp/roboshop.log
 useradd roboshop &>>/tmp/roboshop.log
 
-echo "\e[33m  create directory \e[0m"
+echo -e "\e[33m  create directory \e[0m"
 create directory &>>/tmp/roboshop.log
 mkdir /app &>>/tmp/roboshop.log
 
-echo "\e[33m Download application code \e[0m"
+echo -e "\e[33m Download application code \e[0m"
  Download the application code to created app directory.
  curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
  cd /app &>>/tmp/roboshop.log
  unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
 
- echo "\e[33m download the dependencies \e[0m"
+ echo -e "\e[33m download the dependencies \e[0m"
  cd /app &>>/tmp/roboshop.log
  npm install &>>/tmp/roboshop.log
 
-echo "\e[33m copy the catalog service and update \e[0m"
+echo -e "\e[33m copy the catalog service and update \e[0m"
  cp catalogue.service /etc/systemd/system/catalogue.service
 
- echo "\e[33m Load the service \e[0m"
+ echo -e "\e[33m Load the service \e[0m"
  systemctl daemon-reload &>>/tmp/roboshop.log
 
-echo "\e[33m Start the service \e[0m"
+echo -e "\e[33m Start the service \e[0m"
  systemctl enable catalogue
  systemctl start catalogue
 

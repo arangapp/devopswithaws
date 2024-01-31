f3167492aabed40dd1031971db0601f2cb92682e
anjin <awsdpr.anji01@gmail.com>
2024-02-01 04:13:10 +0800

    frontend config

--- roboshop-shell-auto/frontend.sh
+++ roboshop-shell-auto/frontend.sh
@@ -1,22 +1,24 @@
 
 echo -e "\e[33m Install Nginx \e[0m"
-yum install nginx -y
+yum install nginx -y &&>>/tmp/roboshop.log
 
 echo -e "\e[33m Remove the default content \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &&>>/tmp/roboshop.log
 
 echo -e "\e[33m Download the frontend content \e[0m"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &&>>/tmp/roboshop.log
 
 echo -e "\e[33m Extract the frontend content \e[0m"
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+cd /usr/share/nginx/html &&>>/tmp/roboshop.log
+unzip /tmp/frontend.zip &&>>/tmp/roboshop.log
 
 echo -e "\e[33m Create Nginx Reverse Proxy Configuration \e[0m"
 ## we need to copy config file
-##vim /etc/nginx/default.d/roboshop.conf
+
+echo -e "\e[33m update frontend config \e[0m"
+cp roboshop.confif /etc/nginx/default.d/roboshop.conf &&>>/tmp/roboshop.log
 
 echo -e "\e[33m restart & Enable Nginx service \e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &&>>/tmp/roboshop.log
+systemctl restart nginx &&>>/tmp/roboshop.log

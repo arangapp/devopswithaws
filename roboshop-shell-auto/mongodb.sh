29557deb38c003bbc2c62466f94041de02559d66
anjin <awsdpr.anji01@gmail.com>
2024-02-01 04:21:46 +0800

    mongodb

--- roboshop-shell-auto/mongodb.sh
+++ roboshop-shell-auto/mongodb.sh
@@ -1,13 +1,13 @@
 echo -e "\e[33m Setup the MongoDB repo file \e[0m"
-cp mongo.repo /etc/yum.repos.d/mongo.repo &&>>/tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[33m Install MongoDB \e[0m"
-yum install mongodb-org -y &&>>/tmp/roboshop.log
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m Update listen address \e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &&>>/tmp/roboshop.log
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>/tmp/roboshop.log
 
 echo -e "\e[33m enable & Restart the service \e[0m"
 
-systemctl enable mongod  &&>>/tmp/roboshop.log
-systemctl restart mongod &&>>/tmp/roboshop.log
+systemctl enable mongod  &>>/tmp/roboshop.log
+systemctl restart mongod &>>/tmp/roboshop.log

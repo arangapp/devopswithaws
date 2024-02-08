ae04a262a36f9479e2176dc1ca71841ca25cac0d
anjin <awsdpr.anji01@gmail.com>
2024-02-08 18:44:45 +0800

    catalogue script and config

--- roboshop-shell-auto/catalogue.sh
+++ roboshop-shell-auto/catalogue.sh
@@ -1,10 +1,10 @@
 
 echo -e "\e[33m Module list \e[0m"
-dnf module disable nodejs -y &>>/tmp/roboshop.log
-dnf module enable nodejs:18 -y &>>/tmp/roboshop.log
+yum module disable nodejs -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+yum module enable nodejs:18 -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
 
 echo -e "\e[33m  install NodeJs \e[0m"
-dnf install nodejs -y &>>/tmp/roboshop.log
+yum install nodejs -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
 
 echo -e "\e[33m  add app user \e[0m"
 rm -rf useradd &>>/tmp/roboshop.log
@@ -15,24 +15,24 @@ create directory &>>/tmp/roboshop.log
 mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[33m Download application code \e[0m"
- curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>/tmp/roboshop.log
- cd /app &>>/tmp/roboshop.log
- unzip /tmp/catalogue.zip &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip  &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip &>>/tmp/roboshop.log &>>/tmp/roboshop.log
 
- echo -e "\e[33m download the dependencies \e[0m"
- cd /app &>>/tmp/roboshop.log
- npm install &>>/tmp/roboshop.log
+echo -e "\e[33m download the dependencies \e[0m"
+cd /app &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+npm install &>>/tmp/roboshop.log &>>/tmp/roboshop.log
 
 echo -e "\e[33m copy the catalog service and update \e[0m"
- cp catalogue.service /etc/systemd/system/catalogue.service
+cp catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log &>>/tmp/roboshop.log
 
- echo -e "\e[33m Load the service \e[0m"
- systemctl daemon-reload &>>/tmp/roboshop.log
+echo -e "\e[33m Load the service \e[0m"
+systemctl daemon-reload &>>/tmp/roboshop.log &>>/tmp/roboshop.log
 
 echo -e "\e[33m Start the service \e[0m"
- systemctl enable catalogue
- systemctl start catalogue
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl restart catalogue &>>/tmp/roboshop.log
 
- echo -e "\e[33m Setup the MongoDB repo file \e[0m"
- cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
- dnf install mongodb-org-shell -y &>>/tmp/roboshop.log
+echo -e "\e[33m Setup the MongoDB repo file \e[0m"
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log &>>/tmp/roboshop.log
